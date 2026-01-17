31052d126724cbecba452998bd69363d895078ad
doanquangkien <95895875+doanquangkien@users.noreply.github.com>
2026-01-17 14:20:20 +0700

    Create marketing.js

---
+++ js/controllers/marketing.js
@@ -0,0 +1,7 @@
+function marketingController() {
+    return {
+        coupons: [
+            {code: 'SALE50', type: 'percent', value: 50, uses: 15, maxUses: 100, expiry: '2026-12-30'},
+        ]
+    }
+}
