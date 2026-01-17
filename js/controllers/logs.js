c8f1f7b633b72d6e97b09f0b8506edbaa949cb5c
doanquangkien <95895875+doanquangkien@users.noreply.github.com>
2026-01-17 14:22:46 +0700

    Create logs.js

---
+++ js/controllers/logs.js
@@ -0,0 +1,8 @@
+function logsController() {
+    return {
+        logs: [
+            {time: '10:00:01', action: 'System started.'},
+            {time: '10:05:22', action: 'Admin logged in (IP: 192.168.1.1).'},
+        ]
+    }
+}
