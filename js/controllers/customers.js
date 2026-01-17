8783d25d1b8f2edc4327a3fd8519f352c351557e
doanquangkien <95895875+doanquangkien@users.noreply.github.com>
2026-01-17 14:19:53 +0700

    Create customers.js

---
+++ js/controllers/customers.js
@@ -0,0 +1,7 @@
+function customersController() {
+    return {
+        customers: [
+            {email: 'tuan.nguyen@test.com', phone: '0123456789', orders: 12, spent: 1500000, ip: '192.168.1.1', userAgent: 'Mozilla/5.0', utm: 'google'},
+        ]
+    }
+}
